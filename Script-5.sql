df30f990d8749c0bbf6331a58d75872c5b6a799e
unknown <Administrador@gi02tads85.integrado.int>
2024-08-19 22:01:18 -0300

    Backflip V0.3

--- Script-5.sql
+++ Script-5.sql
@@ -1,27 +1,46 @@
 create table Cenas(
-Id Integer,
-Id_iten Integer,
+Id_cenas Int not null primary key,
+Id_itens Integer,
 titulo Varchar(50),
-descricao Varchar(500)
+descricao Varchar(500),
+foreign key (Id_itens) references itens(Id_itens)
 )
 
 create table itens(
-Id Integer,
+Id_itens Int not null primary key,
 Id_Cena Integer,
 Descricao Varchar (100),
 Erro Varchar(50),
 Interagir Varchar(100),
-Carregavel Boolean
+Carregavel Boolean,
+foreign key (Id_cenas) references Cenas(Id_cenas)
 )
 
 create table Pessoas(
-Id Integer,
+Id_pessoas Int not null primary key,
 nome Varchar(50),
 Comando Varchar(50),
 Descricao Varchar(100),
 Erro Varchar(200)
 )
 
+create table comando(
+Id_comando Int not null primary key,
+comandouse ()
+)
+
+create table saveGame (
+Id_save Int not null primary key,
+Id_cenas Int,
+foreign key (Id_cenas) references Cenas(Id_cenas)
+)
+
+create table itens_save(
+Id_itens, 
+Id_save,
+foreign key (Id_itens) references itens(Id_itens),
+foreign key (Id_save) references saveGame(Id_save)
+)
 
 insert into cenas(Id,titulo,descricao) values (0,'Introdução','O grande inventor jhon backflip foi morto a sangue frio precisamos descobrir quem fez tal atrocidade');
 insert into cenas(Id,titulo,descricao) values (1,'Invetigação','Estamos cada vez mais perto do culpado e achamos uma passagem secreta ao puxar um dos livros a passagem leva até uma catacumba sombria');
